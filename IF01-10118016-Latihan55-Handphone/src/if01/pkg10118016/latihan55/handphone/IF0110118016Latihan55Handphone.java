b812f043a8e35c7bd45c19d0729fe8663f063fb7
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:54:07 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan55-Handphone/src/if01/pkg10118016/latihan55/handphone/IF0110118016Latihan55Handphone.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan55.handphone;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Handphone
+ */
+
+public class IF0110118016Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Android android = new Android("Samsung", "Android", "Grand", 3000000);
+        android.displayProduct();
+        android.setKeyStore("234ibfd3840fo");
+        System.out.println("Andriod Key Store : " + android.getKeyStore());
+
+        BlackBerry blackBerry = new BlackBerry("BlackB", "RIM", "Curve", 2000000);
+        blackBerry.displayProduct();
+        blackBerry.setPinBB("BHS249");
+        System.out.println("PIN : " + blackBerry.getPinBB());
+
+        WindowsPhone windowsPhone = new WindowsPhone("Nokia", "Win8", "Lumia", 1000000);
+        windowsPhone.displayProduct();
+        windowsPhone.setWpKeyStore("UUUQIJWORJ");
+        System.out.println("WP Key Store : " + windowsPhone.getWpKeyStore());
+    }
+    
+}
